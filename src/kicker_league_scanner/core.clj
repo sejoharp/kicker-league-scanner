dc6643bf9f484021768344ab468fce287827e15d
Joscha <zippelman@web.de>
2024-03-23 16:01:12 +0100

    parse match links

--- src/kicker_league_scanner/core.clj
+++ src/kicker_league_scanner/core.clj
@@ -4,25 +4,42 @@
             [hickory.select :as s])
 
   (:gen-class))
-(defn parse-links [parsed-html]
-  (let [html (slurp "test/resources/league-overview.html")
-        parsed-html (h/as-hickory (h/parse html))
-        links (s/select [{:tag :a}] parsed-html)]
-    (for [link links
-          :let [text (get-in link [:content 0 :content])
-                href (get-in link [:attrs :href])]
-          :when (= text "Begegnungen …")]
-      href)))
 
-(defn read-league-overview [overview-link]
+(defn html->hickory [overview-link]
   (let [html (slurp overview-link)]
     (h/as-hickory (h/parse html))))
 
-(defn parse-game-links [parsed-html]
-  (let [link-htmls (s/select (s/descendant (s/class "readon")) parsed-html)
-        begegnungen-links (filter #(str/includes? (:content %) "Begegnungen …") link-htmls)
-        links (map #(get-in % [:attrs :href]) begegnungen-links)]
+(defn get-leagues [])
+
+(defn get-league-links-from-league-overview [parsed-html]
+  (let [link-htmls (s/select (s/descendant (s/class "readon"))
+                             parsed-html)
+        begegnungen-links (filter #(str/includes? (:content %) "Begegnungen …")
+                                  link-htmls)
+        links (map #(get-in % [:attrs :href])
+                   begegnungen-links)]
     links))
+
+(defn completed-match? [[actual-result & remaining]]
+  (let [first-character (first actual-result)]
+    (contains? (set "0123456789") first-character)))
+
+(defn get-match-links-from-league [parsed-html]
+  (let [link-maps (s/select (s/descendant (s/or (s/class "sectiontableentry1")
+                                                (s/class "sectiontableentry2"))
+                                          (s/tag :a))
+                            parsed-html)]
+    (->> link-maps
+         (filter #(and
+                    (completed-match? (:content %))
+                    (some? (get-in % [:attrs :href]))
+                    (str/includes? (get-in % [:attrs :href])
+                                   "begegnung_spielplan")
+                    ))
+         (map #(get-in % [:attrs :href]))
+         )))
+
+(defn parse-games-from-match [parsed-html])
 
 (defn -main []
   (println "Hello, World!"))
--- test/kicker_league_scanner/core_test.clj
+++ test/kicker_league_scanner/core_test.clj
@@ -1,16 +1,14 @@
 (ns kicker-league-scanner.core-test
   (:require [clojure.test :refer :all]
-            [hickory.select :as s]
             [kicker-league-scanner.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
-
-(deftest ^:unit ^:test-refresh/focus league-overview
-  (let [parsed-html (read-league-overview "test/resources/league-overview.html")]
+(deftest html->hickory-test
+  (let [parsed-html (html->hickory "test/resources/league.html")]
     (is (= :document
-           (:type parsed-html)))
+           (:type parsed-html)))))
+
+(deftest league-overview
+  (let [parsed-html (html->hickory "test/resources/league-overview.html")]
     (is (= ["/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=228"
             "/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=229"
             "/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=230"
@@ -21,4 +19,70 @@
             "/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=235"
             "/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=236"
             "/liga/ergebnisse-und-tabellen?task=veranstaltung&veranstaltungid=237"]
-           (parse-game-links parsed-html)))))
+           (get-league-links-from-league-overview parsed-html)))))
+
+
+(deftest completed-match-test
+  (is (= true
+         (completed-match? ["28:4"])))
+  (is (= false
+         (completed-match? ["_:_"]))))
+(deftest ^:test-refresh/focus league-matches
+  (let [parsed-html (html->hickory "test/resources/league.html")]
+    (is (= ["/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15018"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15014"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15020"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15016"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15024"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15030"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15026"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15028"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15022"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15034"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15032"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15040"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15038"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15036"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15044"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15042"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15050"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15046"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15048"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15054"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15058"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15056"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15052"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15060"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15062"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15070"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15066"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15068"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15064"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15072"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15076"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15080"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15078"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15074"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15084"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15090"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15086"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15088"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15082"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15096"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15092"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15100"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15098"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15094"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15019"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15013"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15015"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15021"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15017"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15025"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15023"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15029"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15031"
+            "/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15027"]
+
+           (get-match-links-from-league parsed-html)))))
