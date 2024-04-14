dcae4a41fffa8948b156f8d0ab86854e1583db8d
Joscha <zippelman@web.de>
2024-04-14 10:38:21 +0200

    Revert "fix links in tests"
    This reverts commit a1068262221a1bd0fba9b996457e8025e9d205e6.

--- test/kicker_league_scanner/io_test.clj
+++ test/kicker_league_scanner/io_test.clj
@@ -5,9 +5,9 @@
 
 (deftest checks-file-existence
   (is (false?
-        (new-match? "test/resources" "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=15478")))
+        (new-match? "test/resources" "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=15478")))
   (is (true?
-        (new-match? "test/resources" "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=non-existant"))))
+        (new-match? "test/resources" "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=non-existant"))))
 
 (deftest saves-match-to-csv-file
   (let [match {:date       "2023-09-05"
@@ -15,7 +15,7 @@
                :guest-team "Kickertrupp (NR)"
                :games      [{:home {:names ["Felix"] :score 6} :guest {:names ["Samuel"] :score 2} :position 1}
                             {:home {:names ["Walter" "Felix"] :score 6} :guest {:names ["Samuel" "Boran"] :score 4} :position 4}]
-               :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+               :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                :match-day  1}
         expected-csv-content (str (str/join "\n"
                                             ["2023-09-05;1;1;H;Flying Circus;Felix;XXXX;6;2;Samuel;XXXX;Kickertrupp (NR);G;2;0;2023/03;1"
@@ -40,7 +40,7 @@
                :guest-team "Kickertrupp (NR)"
                :games      [{:home {:names ["Felix"] :score 6} :guest {:names ["Samuel"] :score 2} :position 1}
                             {:home {:names ["Walter" "Felix"] :score 6} :guest {:names ["Samuel" "Boran"] :score 4} :position 4}]
-               :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+               :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                :match-day  1}
         path "test/resources/task=begegnung_spielplan-veranstaltungid=229-id=15012.edn"]
     (delete-file path)
@@ -77,7 +77,7 @@
                :guest-team "Kickertrupp (NR)"
                :games      [{:home {:names ["Felix"] :score 6} :guest {:names ["Samuel"] :score 2} :position 1}
                             {:home {:names ["Walter" "Felix"] :score 6} :guest {:names ["Samuel" "Boran"] :score 4} :position 4}]
-               :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+               :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                :match-day  1}
         external-test-match {:date       "2023-10-12"
                              :home-team  "Anboard"
@@ -106,7 +106,7 @@
 
 (deftest transforms-link-to-filename
   (is (= "task=begegnung_spielplan-veranstaltungid=237-id=15478.edn"
-         (link->filename "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=15478"))))
+         (link->filename "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=237&id=15478"))))
 
 
 (deftest calculate-game-points-test
@@ -137,21 +137,21 @@
                                       {:home {:names ["Walter"] :score 2} :guest {:names ["Yasmine"] :score 6} :position 2}
                                       {:home {:names ["Walter" "Felix"] :score 3} :guest {:names ["Samuel" "Boran"] :score 6} :position 3}
                                       {:home {:names ["Walter" "Felix"] :score 6} :guest {:names ["Samuel" "Boran"] :score 4} :position 4}]
-                         :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+                         :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                          :match-day  1}
         match-tied {:date       "2023-09-05"
                     :home-team  "Flying Circus"
                     :guest-team "Kickertrupp (NR)"
                     :games      [{:home {:names ["Felix"] :score 6} :guest {:names ["Samuel"] :score 2} :position 1}
                                  {:home {:names ["Walter"] :score 2} :guest {:names ["Yasmine"] :score 6} :position 2}]
-                    :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+                    :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                     :match-day  1}
         match-home-won {:date       "2023-09-05"
                         :home-team  "Flying Circus"
                         :guest-team "Kickertrupp (NR)"
                         :games      [{:home {:names ["Felix"] :score 6} :guest {:names ["Samuel"] :score 2} :position 1}
                                      {:home {:names ["Walter"] :score 6} :guest {:names ["Yasmine"] :score 0} :position 2}]
-                        :link       "https://kickern-hamburg.de/liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
+                        :link       "https://kickern-hamburg.de//liga/ergebnisse-und-tabellen?task=begegnung_spielplan&veranstaltungid=229&id=15012"
                         :match-day  1}]
     (is (= [0 2]
            (calculate-points match-guest-won)))
