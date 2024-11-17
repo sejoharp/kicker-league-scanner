2469af1a3f263f8287b07f86bf92c58ea8133534
Joscha <zippelman@web.de>
2024-11-17 19:37:42 +0000

    fix openrc service scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /root/kicker-league-scanner
 source .env
-./lein.sh run server
+./lein.sh uberjar
+java -XX:+UseParallelGC -jar target/kicker-league-scanner-1.0.0-standalone.jar server
 
